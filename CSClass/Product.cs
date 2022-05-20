b467727c47a96ebccb79a0b23554a717148bcfd6
Serin Kim <serin4723@naver.com>
2022-05-20 11:18:29 +0900

    #4 클래스 인스턴스 변수와 클래스 변수
    C#은 인스턴스 변수 생성과 동시에 초기화가 가능하며, static을 통해 클래스 변수를 선언한다.

---
+++ CSClass/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace CSClass
+{
+    public class Product
+    {
+        public static string MADEIN = "Korea";
+        
+        ///<summary>
+        ///상품 이름
+        ///</summary>
+        public string name = "빵";
+
+        ///<summary>
+        ///상품 가격(단위 : 원)
+        ///</summary>
+        public int price = 1000;
+    }
+}
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -53,6 +53,18 @@ namespace CSClass
             Console.WriteLine(Math.Round(52.273));    //52
             Console.WriteLine(Math.PI);
 
+            //C#에서는 파일명과 클래스명이 달라도 괜찮지만 지양한다.
+            Hamburger yummy = new Hamburger();
+
+            Product productA = new Product();
+            productA.name = "포켓몬빵";
+            productA.price = 1500;
+
+            //인스턴스 변수를 생성과 동시에 초기화가 가능하다(C#의 고유 문법)
+            Product productB = new Product() { name = "소금빵", price = 2000 };
+            Product productC = new Product() { price = 15000, name = "당근케이크" };
+            Product productD = new Product() { name = "보름달" };
+            Product productE = new Product() { price = 30000 };
 
         }
     }
