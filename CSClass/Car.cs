9c54ed2875d4105ef426dc9ab2c2ce3b53385ffc
Serin Kim <serin4723@naver.com>
2022-05-20 10:04:58 +0900

    #2 Math 클래스 사용 및 주석 처리

--- CSClass/Car.cs
+++ CSClass/Car.cs
@@ -2,18 +2,36 @@
 
 namespace CSClass
 {
+    ///<summary>
+    ///자동차 주차장 입차, 출차 시간 등을 저장하는 클래스 입니다.
+    ///</summary>
     internal class Car
     {
         int carNumber;
         DateTime inTime;
         DateTime outTime;
-       
 
+        ///<summary>
+        ///무엇인가를 입력받고 출력합니다.
+        ///</summary>
+        ///<param name="any">입력받는 변수</param>
+        ///<returns>반환되는 값(입력변수 * 10)</returns>
+        public int some(int any)
+        {
+            return any * 10;
+        }
+
+        ///<summary>
+        ///자동차 입차 시간을 지정합니다.
+        ///</summary>
         public void SetInTime()
         {
             this.inTime = DateTime.Now;
         }
 
+        ///<summary>
+        ///자동차 출차 시간을 지정합니다.
+        ///</summary>
         public void SetOUtTime()
         {
             this.outTime = DateTime.Now;
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -44,6 +44,16 @@ namespace CSClass
             {
                 Console.WriteLine("Count : " + list.Count + "\t item: " + item);
             }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));    //53
+            Console.WriteLine(Math.Floor(52.273));    //52
+            Console.WriteLine(Math.Max(52, 273));    //273
+            Console.WriteLine(Math.Min(52, 273));    //52
+            Console.WriteLine(Math.Round(52.273));    //52
+            Console.WriteLine(Math.PI);
+
+
         }
     }
 }
